c67b909f09099918be39fb59273c926287eb7460
Maki <haru.maki.ch.0303@gmail.com>
2024-03-30 18:41:13 +0900

    ソースコードの忽視対象ファイルリストを更新し、変更履歴生成機能を追加
    - .SourceSageignoreファイルに"Changelog"ディレクトリを除外対象に追加
    - ソースコード解析結果の出力ディレクトリを"docs/SourceSage.md"に変更
    - 変更履歴生成機能を追加し、"docs/Changelog"ディレクトリへの出力を実装
    
    これにより、ソースコード解析結果と変更履歴をドキュメント化して管理できるようになり、開発プロセスの透明性と可視性が向上します。

--- SourceSage.py
+++ SourceSage.py
@@ -8,10 +8,20 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'modules'))
 pprint.pprint(sys.path)
 
 from modules.source_sage import SourceSage
+from modules.ChangelogGenerator import ChangelogGenerator
 
 if __name__ == "__main__":
     folders = ['./']
     source_sage = SourceSage(folders, ignore_file='.SourceSageignore',
-                             output_file='SourceSage.md',
+                             output_file='docs/SourceSage.md',
                              language_map_file='config/language_map.json')
     source_sage.generate_markdown()
+
+
+    repo_path = "./"
+    output_dir = "docs/Changelog"
+    os.makedirs(output_dir, exist_ok=True)  # ディレクトリが存在しない場合は作成する
+
+    generator = ChangelogGenerator(repo_path, output_dir)
+    generator.generate_changelog_for_all_branches()
+    generator.integrate_changelogs()
