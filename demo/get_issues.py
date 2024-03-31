277db1041f87ec0c964aa5b8e645259293ed3efd
Maki <haru.maki.ch.0303@gmail.com>
2024-03-31 11:55:18 +0900

    開いているIssueのデータを保存し、フィルタリングした結果を別ファイルとして保存する機能を追加しました。これにより、開発者がIssueの状況を詳細に把握しやすくなり、プロジェクト管理が効率化されます。

--- demo/get_issues.py
+++ demo/get_issues.py
@@ -7,6 +7,7 @@ owner = "Sunwood-ai-labs"
 repository = "SourceSage"
 save_path = "SourceSageAssets"  # 保存先のディレクトリパス
 file_name = "open_issues_filtered.json"  # 保存するファイル名
+file_name_full = "open_issues_full.json"  # 保存するファイル名
 
 # GitHub APIのURLを構築（開いているIssueを全て取得する）
 api_url = f'https://api.github.com/repos/{owner}/{repository}/issues?state=open'
@@ -29,5 +30,9 @@ if not os.path.exists(save_path):
 # 取得したIssueデータをJSON形式で保存
 with open(os.path.join(save_path, file_name), 'w', encoding='utf-8') as f:
     json.dump(filtered_issues, f, ensure_ascii=False, indent=4)
+
+# 取得したIssueデータをJSON形式で保存
+with open(os.path.join(save_path, file_name_full), 'w', encoding='utf-8') as f:
+    json.dump(issues, f, ensure_ascii=False, indent=4)
 
 print(f'Filtered open issues saved to {os.path.join(save_path, file_name)}')
