5d8bf2fd442355b0fa58689e8490691882297984
Maki <sunwood.ai.labs@gmail.com>
2024-04-30 14:18:40 +0900

    [refactor] setup.pyのインデントと依存関係読み込み処理を整理
    `setup.py` ファイルの改善点を以下の通りにまとめました：
    
    - **インデントの調整**: コードの可読性を向上させるため、一部のインデントを調整しました。
    - **依存関係の動的読み込み**: `requirements.txt` から依存関係を動的に読み込むように変更しました。これにより、依存関係の管理が簡略化され、更新が容易になります。
    
    これらの変更により、パッケージ設定の一貫性と保守性が向上します。

--- setup.py
+++ setup.py
@@ -3,33 +3,32 @@ from setuptools import setup, find_packages
 
 # READMEファイルの内容を読み込む
 with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+   long_description = fh.read()
+
+# requirements.txtファイルの内容を読み込む
+with open('requirements.txt') as f:
+   required = f.read().splitlines()
 
 setup(
-    name='sourcesage',
-    version='4.1.1',
-    packages=find_packages(),
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Topic :: Utilities",
-    ],
-    package_data={
-        'sourcesage': ['config/**/**', 
-                       'config/.SourceSageignore'],
-    },
-    entry_points={
-        'console_scripts': [
-            'sourcesage=sourcesage.cli:main',
-        ],
-    },
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Sunwood-ai-labs/SourceSage",
-    install_requires=[
-        'loguru',
-        'GitPython',
-        'requests',
-        'art',
-    ],
+   name='sourcesage',
+   version='4.1.1',
+   packages=find_packages(),
+   classifiers=[
+       "Development Status :: 4 - Beta",
+       "Intended Audience :: Developers",
+       "Topic :: Utilities",
+   ],
+   package_data={
+       'sourcesage': ['config/**/**', 
+                      'config/.SourceSageignore'],
+   },
+   entry_points={
+       'console_scripts': [
+           'sourcesage=sourcesage.cli:main',
+       ],
+   },
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   url="https://github.com/Sunwood-ai-labs/SourceSage",
+   install_requires=required,
 )
