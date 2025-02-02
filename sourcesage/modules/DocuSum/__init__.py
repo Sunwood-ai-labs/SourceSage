489d1931e11d6b1c6e38423b715671d493adcc08
Maki <sunwood.ai.labs@gmail.com>
2025-02-02 10:53:32 +0900

    🌱 feat: DocuSum モジュールの初期化
    - DocuSum モジュールに必要なクラスをインポートしました。
    - `__version__` と `__all__` を定義して、モジュールのバージョンと公開される要素を指定しました。

---
+++ sourcesage/modules/DocuSum/__init__.py
@@ -0,0 +1,14 @@
+from .docusum import DocuSum
+from .file_pattern_matcher import FilePatternMatcher
+from .language_detector import LanguageDetector
+from .tree_generator import TreeGenerator
+from .file_processor import FileProcessor
+
+__version__ = '1.0.0'
+__all__ = [
+    'DocuSum',
+    'FilePatternMatcher',
+    'LanguageDetector',
+    'TreeGenerator',
+    'FileProcessor'
+]
