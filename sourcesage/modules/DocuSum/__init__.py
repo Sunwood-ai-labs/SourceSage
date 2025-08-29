92ec24256b4e56a3858900511a5dfd5f6523ffef
maki <sunwood.ai.labs@gmail.com>
2025-08-29 13:45:46 +0000

    📚 DocuSumコアモジュール - スタイル統一
    - docusum.pyと__init__.pyのクォート統一
    - 引数パラメータのデフォルト値を改善
    - モジュールの構造整理

--- sourcesage/modules/DocuSum/__init__.py
+++ sourcesage/modules/DocuSum/__init__.py
@@ -1,14 +1,14 @@
 from .docusum import DocuSum
 from .file_pattern_matcher import FilePatternMatcher
+from .file_processor import FileProcessor
 from .language_detector import LanguageDetector
 from .tree_generator import TreeGenerator
-from .file_processor import FileProcessor
 
-__version__ = '1.0.0'
+__version__ = "1.0.0"
 __all__ = [
-    'DocuSum',
-    'FilePatternMatcher',
-    'LanguageDetector',
-    'TreeGenerator',
-    'FileProcessor'
+    "DocuSum",
+    "FilePatternMatcher",
+    "LanguageDetector",
+    "TreeGenerator",
+    "FileProcessor",
 ]
